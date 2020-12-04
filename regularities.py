dcea30013949a7a391eb21d5660a003398e8b255
Steven Tanner <tanners2@wwu.edu>
2020-12-03 21:47:09 -0800

    added and unit tested basicMinusConditionTest method which passed minor testing. All generated conditions tested are valid

--- regularities.py
+++ regularities.py
@@ -1,17 +1,53 @@
+#tests the given minus condition against all rows of data, returns true if no line violates the condition
+def basicMinusConditionTest(condition, data, predicates, thisEffect):
+    effectIndex = 0
+    for predicate in predicates:
+        if predicate == thisEffect:
+            break
+        effectIndex += 1
+
+    for lineNumber in range(len(data)):
+        if lineNumber > 0:
+            lineElements = data[lineNumber].split()
+            #only check rows where effect is false
+            if int(lineElements[effectIndex + 1]) == -1:
+                #if all inus are met, the minus condition is proven false
+                inusMet = 0
+                for inus in condition:
+                    elementNumber = 0
+                    for lineElement in lineElements:
+                        if elementNumber > 0:
+                            #negative inus
+                            if list(inus)[0] == "~":
+                                if list(inus)[1] == predicates[elementNumber - 1] and int(lineElement) < 0:
+                                    inusMet += 1
+                            #positive inus
+                            else:
+                                if list(inus)[0] == predicates[elementNumber - 1] and int(lineElement) > 0:
+                                    inusMet += 1
+                        elementNumber += 1
+                if inusMet == len(condition):
+                    return False
+
+    return True
+
+
 
 #load data
 #fileName = input("Enter the name of your dataset: ")
 fileName = 'testFile.txt'
-dataSet = open(fileName, "r")
+dataReader = open(fileName, "r")
+dataSet = {}
 lineCount = 0
 columns = {}
-for line in dataSet :
+effects = {}
+for line in dataReader :
+    dataSet[lineCount] = line
     if lineCount == 0 :
         effects = line.split()
-    else :
+    else:
         newDict = {}
         newline = line.split()
-        columnArray = []
         elementCounter = 0
         for element in newline :
             if elementCounter != 0 :
@@ -28,7 +64,7 @@ chosen_effect = input("Enter the desired effect: ")
 minus = []
 for rowVal1, rowData1 in columns.items():
     for rowVal2, rowData2 in columns.items():
-        if rowData1[chosen_effect] == rowData2[chosen_effect] and rowVal1 != rowVal2 and rowData1[chosen_effect] > 0 and  rowData2[chosen_effect] > 0:
+        if rowData1[chosen_effect] == rowData2[chosen_effect] and rowVal1 != rowVal2 and rowData1[chosen_effect] > 0 and rowData2[chosen_effect] > 0:
             matches = set()
             for effect in effects:
                 if rowData1[effect] == rowData2[effect] and effect != chosen_effect:
@@ -44,8 +80,9 @@ for rowVal1, rowData1 in columns.items():
                     break
                 if matches.issubset(conditions) :
                     minus.remove(conditions)
-            if valid == True:
-                minus.append(matches)        
+            if valid:
+                if basicMinusConditionTest(matches, dataSet, effects, chosen_effect):
+                    minus.append(matches)
                     
                     
 print(minus)

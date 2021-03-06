f5eb5d81a8b2f0d7472ebcc6cb41b7757c8cb69d
thomasbidinger <tbid20@gmail.com>
2021-03-05 21:47:44 -0800

    changes made during coding meeting on thursday

--- regularities.py
+++ regularities.py
@@ -1,3 +1,5 @@
+from itertools import chain, combinations
+
 def necessaryCheck(minus, data, predicates, chosen) :
     newMinus = []
     for condition in minus:
@@ -73,96 +75,103 @@ def getDataset(fileName):
     # print(columns)
 
     return dataSet, predicates, rows
+    
+def find_all_subsets(rows, chosenEffect):
+    positive_rows = []
+    subsets = []
+    
+    for row in rows:
+        if row[chosen_effect] == 1:
+            s = list(row)
+            rowSubsets = chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+            subsets.append(rowSubsets)
+    
+    return subsets
+       
 
 if __name__ == '__main__':
     # fileName = input("Enter the name of your dataset: ")
     fileName = 'testFile.txt'
     dataSet, predicates, rows = getDataset(fileName)
 
-    chosen_effect = input("Enter the desired effect: ")
+    #TODO: turn dataset into "~P, Q, ~R" format and fix find_all_subsets to remove subsets w/ effect and duplicates
 
+    chosen_effect = input("Enter the desired effect: ")
+    print("rows")
+    for row in rows:
+        print(rows.get(row))
+    untestedConditions = find_all_subsets(rows, chosen_effect)
 
     minus = []
-    #print(columns.items())
-    positive_row = []
-
     for rowVal1, rowData1 in rows.items():
-        if rowData1[chosen_effect] == 1:
-            positive_row.append(rowVal1)
+        for rowVal2, rowData2 in rows.items():
+            if rowData1[chosen_effect] > 0 and rowData2[chosen_effect] > 0 and rowVal1 != rowVal2:
+                matches = set()
+                
+                # if rowData1[predicate] < 0:
+                #     matches.add(('~' + predicate))
+                # else:
+                #     matches.add(predicate)
+                #don't add if a super-set
+                valid = True
+                minus_temp = minus.copy()
+                for conditions in minus_temp:
+                    print("matches")
+                    print(matches)
+                    if matches.issuperset(conditions):
+                        valid = False
+                        break
+                    #checks if previously added elements are supersets
+                    if matches.issubset(conditions) :
+                        minus.remove(conditions)
+                if valid:
+                    if basicMinusConditionTest(matches, dataSet, predicates, chosen_effect):
+                        minus.append(matches)
+print("Result: ",minus)
+#necessaryCheck(minus, dataSet, predicates, chosen_effect)
 
-    #P: ~Q, T, ~U
-    #R: S, ~Q
+##### To be removed (all below) #####
 
+#     #cur_val = columns[positive_row[0]][effects[0]]
+#     #print(cur_val)
+#     #flag = 0
+#     matchset = set()
+#     for effect in predicates:
+#         if effect != chosen_effect:
+#             cur_val = rows[positive_row[0]][effect] # set value -1 or 1
+#             #matchset = set() #individual matches
+#             flag = 0
+#             #iterate through a columns row, raise flag if values do not equal
+#             for row in positive_row:
+#                 rowMatch = set()
+#                 if flag == 0:
+#                     temp = rows[row][effect]
+#                     if temp != cur_val:
+#                         flag = 1
+#                 #cur_val = temp
+#             if flag == 0:
+#                 if cur_val > 0:
+#                     matchset.add(effect)
+#                 else:
+#                     matchset.add(('~' + effect))
 
-    #cur_val = columns[positive_row[0]][effects[0]]
-    #print(cur_val)
-    #flag = 0
-    matchset = set()
-    for effect in predicates:
-        if effect != chosen_effect:
-            cur_val = rows[positive_row[0]][effect] # set value -1 or 1
-            #matchset = set() #individual matches
-            flag = 0
-            #iterate through a columns row, raise flag if values do not equal
-            for row in positive_row:
-                rowMatch = set()
-                if flag == 0:
-                    temp = rows[row][effect]
-                    if temp != cur_val:
-                        flag = 1
-                #cur_val = temp
-            if flag == 0:
-                if cur_val > 0:
-                    matchset.add(effect)
-                else:
-                    matchset.add(('~' + effect))
+#         matches = set()
+#         for match in matchset:
+#             matches.add(match)
 
-        matches = set()
-        for match in matchset:
-            matches.add(match)
+#         print("Match: ",matchset)
 
-        print("Match: ",matchset)
-
-        #don't add if a super-set
-        valid = True
-        minus_temp = minus.copy()
-        for conditions in minus_temp:
-            #print(matches)
-            if matches.issuperset(conditions):
-                valid = False
-                break
-            #checks if previously added elements are supersets
-            if matches.issubset(conditions) :
-                minus.remove(conditions)
-        if valid:
-            if basicMinusConditionTest(matches, dataSet, predicates, chosen_effect):
-                minus.append(matches)
-
-    print("Result: ",minus)
-
-    necessaryCheck(minus, dataSet, predicates, chosen_effect)
-
-#     for rowVal2, rowData2 in columns.items():
-#         if rowData1[chosen_effect] > 0 and rowData2[chosen_effect] > 0 and rowVal1 != rowVal2:
-#             matches = set()
-#             for effect in effects:
-#                 if rowData1[effect] == rowData2[effect] and effect != chosen_effect:
-#                     if rowData1[effect] < 0:
-#                         matches.add(('~' + effect))
-#                     else:
-#                         matches.add(effect)
-#             #don't add if a super-set
-#             valid = True
-#             minus_temp = minus.copy()
-#             for conditions in minus_temp:
-#                 print(matches)
-#                 if matches.issuperset(conditions):
-#                     valid = False
-#                     break
-#                 #checks if previously added elements are supersets
-#                 if matches.issubset(conditions) :
-#                     minus.remove(conditions)
-#             if valid:
-#                 if basicMinusConditionTest(matches, dataSet, effects, chosen_effect):
-#                     minus.append(matches)
-
+#         #don't add if a super-set
+#         valid = True
+#         minus_temp = minus.copy()
+#         for conditions in minus_temp:
+#             #print(matches)
+#             if matches.issuperset(conditions):
+#                 valid = False
+#                 break
+#             #checks if previously added elements are supersets
+#             if matches.issubset(conditions) :
+#                 minus.remove(conditions)
+#         if valid:
+#             if basicMinusConditionTest(matches, dataSet, predicates, chosen_effect):
+#                 minus.append(matches)
